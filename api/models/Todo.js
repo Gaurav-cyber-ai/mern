7324fe870fbb4014bd041c6e801a544cb9410014
Gaurav-cyber-ai <79096106+Gaurav-cyber-ai@users.noreply.github.com>
2022-11-13 23:48:43 +0530

    Files uplaod

---
+++ api/models/Todo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema=mongoose.Schema;
+const TodoSchema=new Schema({
+    text:{
+        type:String,
+        required:true
+    },
+    complete:{
+        type:Boolean,
+        required:false
+    },
+    timestamp:{
+        type:String,
+        default: Date.now()
+    }
+})
+const Todo =mongoose.model("Todo",TodoSchema);
+module.experts=Todo;
